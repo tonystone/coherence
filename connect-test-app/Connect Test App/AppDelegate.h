eb7e86bf06938b22f2f7877227de05b85303cbb6
Tony Stone <tony@mobilegridinc.com>
2014-10-27 18:22:20 -0700

    Initial commit for an implementation of connect-test-app.

---
+++ connect-test-app/Connect Test App/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Connect Example App
+//
+//  Created by Tony Stone on 10/21/14.
+//  Copyright (c) 2014 Mobile Grid, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+    @property (strong, nonatomic) UIWindow *window;
+
+    @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+    @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+    @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    - (void)saveContext;
+    - (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
