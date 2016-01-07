f9ed53dd2dfdc97915ed888f105598b19e4def42
Tony Stone <hatter24@gmail.com>
2016-01-06 17:11:44 -0800

    Modified playground to remove all code accept import.

--- Example/Coherence.playground/Contents.swift
+++ Example/Coherence.playground/Contents.swift
@@ -1,17 +1,13 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-@testable import Coherence
+import Coherence
 
-let metaModel = MetaModel()
 
-let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true)[0] as String
 
-let logWriter = try  WriteAheadLog(url: NSURL(fileURLWithPath: documentsPath))
 
-//var logEntry: MetaLogEntry = NSEntityDescription.entityForName(", inManagedObjectContext: <#T##NSManagedObjectContext#>)
-var currentTimestamp = NSDate().timeIntervalSinceNow
-//logEntry.timestamp = NSDate().timeIntervalSinceNow
+
+
 
 
 
