f10b206794b78a51119f061dcafd3338cc4583ab
Tony Stone <hatter24@gmail.com>
2016-01-06 17:12:17 -0800

    Ran "pod update" and "pod lib lint" to ready it for internal release.

--- Example/Pods/Target Support Files/Coherence/Coherence-umbrella.h
+++ Example/Pods/Target Support Files/Coherence/Coherence-umbrella.h
@@ -1,13 +1,11 @@
 #import <UIKit/UIKit.h>
 
 #import "Coherence.h"
-#import "CCAssert.h"
-#import "CCManagedObjectContext.h"
 #import "CCConfiguration.h"
 #import "CCModule.h"
 #import "CCResource.h"
 #import "CCResourceService.h"
-#import "NSString+CapitalizeFirstLetter.h"
+#import "CCObject.h"
 
 FOUNDATION_EXPORT double CoherenceVersionNumber;
 FOUNDATION_EXPORT const unsigned char CoherenceVersionString[];
