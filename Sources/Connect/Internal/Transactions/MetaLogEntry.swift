7a7556a8b4bba79d6317271da16f28ae4065a6e2
Tony Stone <hatter24@gmail.com>
2017-03-06 14:11:38 -0800

    Removed extra space in description of UpdateData.

--- Sources/Connect/Internal/Transactions/MetaLogEntry.swift
+++ Sources/Connect/Internal/Transactions/MetaLogEntry.swift
@@ -102,7 +102,7 @@ extension MetaLogEntry.InsertData {
 extension MetaLogEntry.UpdateData {
 
     public override var description: String {
-        return "\(String(describing: type(of: self))) { attributesAndValues: \(attributesAndValues?.description ?? "nil") updatedAttributes:  \(updatedAttributes?.description ?? "nil") }"
+        return "\(String(describing: type(of: self))) { attributesAndValues: \(attributesAndValues?.description ?? "nil") updatedAttributes: \(updatedAttributes?.description ?? "nil") }"
     }
 }
 
